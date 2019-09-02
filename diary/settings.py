f9dfd5d7891fbcb2bc6d3d8f99951cd90ee53479
Abhishek Verma <av4programming@gmail.com>
2019-09-02 07:43:26 +0530

    fixing

--- diary/settings.py
+++ diary/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6aow@&3dofto&%0gr172qz8(be97z6@j$68)p$100kx17ev@xw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['diaryislife.herokuapp.com']
 
